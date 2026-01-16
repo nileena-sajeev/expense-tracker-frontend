8a6983e65ea6c391239f1d3044cb05e44f088122
nileena-sajeev <nileenas123@gmail.com>
2026-01-16 19:41:29 +0530

    Home.js

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,6 +1,6 @@
 import { useNavigate } from "react-router-dom";
 import "./Home.css";
-import { motion } from "framer-motion";
+
 
 export default function Home() {
   const navigate = useNavigate();
